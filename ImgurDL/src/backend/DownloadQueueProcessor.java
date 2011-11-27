dfa91162fab330e1fd0953305b364d8c89b11802
Isaac Assegai <confusedvirtuoso@gmail.com>
2011-11-27 15:36:15 -0800

    Test Commit, Run 1

---
+++ ImgurDL/src/backend/DownloadQueueProcessor.java
@@ -0,0 +1,52 @@
+package backend;
+
+/**
+ * A Thread that Processes the Download Queue, by downloading Items in the Queue.
+ * @author Isaac Assegai
+ *
+ */
+public class DownloadQueueProcessor extends Thread{
+	
+	static int TIME_SLEEP_IF_EMPTY = 500;
+	static int TIME_PAUSE = 100;
+	ImgurGalleryDownloader parent;
+	PictureDownloader picDownloader; //The object that downloads a picture
+	
+	
+	/**
+	 * Constructor
+	 */
+	public DownloadQueueProcessor(ImgurGalleryDownloader p){
+		parent = p;
+		picDownloader = new PictureDownloader(this);
+	}
+	
+	/**
+	 * The Processors main loop.
+	 */
+	public void run(){
+		while(parent.isRunning){ //make sure program is still running
+			while(parent.queue.isEmpty()){ 
+				try {
+					Thread.sleep(TIME_SLEEP_IF_EMPTY); //if DL Queue is empty, pause for half a second before trying again
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			if(!parent.queue.isEmpty()){
+				String picToDL = parent.queue.deQueue();
+				picDownloader.download(picToDL);
+			}
+			
+			try {
+				Thread.sleep(TIME_PAUSE); //Allow thread to rest some time, before checking again.
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ ImgurDL/src/gui/InputArea.java
@@ -0,0 +1,127 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.plaf.metal.MetalBorders;
+
+/**
+ * The input area. Includes a textbox and a button.
+ * @author Isaac Assegai
+ *
+ */
+public class InputArea extends JPanel{
+
+	static String TEXTFIELD_STRING = "Input URL To Imgur Gallery.";
+	ImgurDLHeader parent; /** The Header, Parent of this Class. */
+	JTextField textField; /** The Box that user inputs Text into. */
+	DownloadButton button; /** The button you press to download. */
+	boolean beenPressed = false;
+	
+	/**
+	 * Constructor
+	 */
+	public InputArea(ImgurDLHeader p){
+		super();
+		setLayout(new FlowLayout());
+		parent = p;
+		setupInputArea();
+		setupTextField();
+		setupButton();
+		
+	}
+	
+	/**
+	 * Setup Input area's button.
+	 */
+	public void setupButton(){
+		
+		button = new DownloadButton("Download");
+		add(button);
+
+		button.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+            	
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+                String gallery = textField.getText();
+                gallery = gallery.toLowerCase();
+                if(!gallery.startsWith("http")){
+                	parent.parent.parent.mainCanvas.header.inputArea.textField.setText("Not a HTTP address. Try Another");
+                }else{
+                	
+                	if(!beenPressed){
+                		parent.parent.parent.guiManager.downloadGallery(gallery);
+                        beenPressed = true;
+                	}
+                
+                }
+            }
+        });
+	}
+	
+	/**
+	 * Setup The input area.
+	 */
+	public void setupInputArea(){
+		this.setBackground(new Color(43,43,43));
+	}
+	
+	/**
+	 * Setup the input area's textfield.
+	 */
+	public void setupTextField(){
+		
+		textField = new JTextField(20);
+		textField.setPreferredSize(new Dimension(8,30));
+		textField.setBackground(new Color(100, 100, 100));
+		textField.setForeground(Color.WHITE);
+		textField.setBorder(null);
+		textField.setBorder(MetalBorders.getTextFieldBorder());
+		textField.setFont(new Font("Thoma", Font.BOLD, 16));
+		textField.setText(TEXTFIELD_STRING);
+		textField.setHorizontalAlignment(JTextField.CENTER);
+		
+		//textField.setPreferredSize(new Dimension(5,50));
+		add(textField, FlowLayout.LEFT);
+	
+	
+	}
+	
+	/**
+	 * Retrieve a picture from the internet.
+	 * @param s The URL of the picture. STRING
+	 * @return The Buffered image of the picture.
+	 */
+	private BufferedImage getPic(String s){
+		BufferedImage img = null;
+ 	    try {
+			URL u = new URL(s);
+			img = ImageIO.read(u);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 	    return img;
+	}
+}
