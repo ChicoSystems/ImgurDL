a6996e05fad3a28e1e039a691ace636bb4992faa
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-09-05 22:37:41 -0700

    Bumped version to 0.2.5
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- ImgurDL/src/gui/ImgurDLMain.java
+++ ImgurDL/src/gui/ImgurDLMain.java
@@ -183,7 +183,7 @@ public class ImgurDLMain extends JFrame{
 	}
 	
 	public static String getVersion(){
-		return "0.2";
+		return "0.2.5";
 	}
 	
 	public JSONObject apiHome(String endpoint, String urlParameters, String method){
